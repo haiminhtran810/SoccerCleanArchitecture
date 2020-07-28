2280904bee14680b66345e2c7795016ea3a9454d
haiminhtran810 <haiminhtran810@gmail.com>
2020-07-28 21:43:22 +0700

    Task: show error dialog

--- app/src/main/java/com/learn/soccercleanarchitecture/base/BaseFragment.kt
+++ app/src/main/java/com/learn/soccercleanarchitecture/base/BaseFragment.kt
@@ -1,17 +1,20 @@
 package com.learn.soccercleanarchitecture.base
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.LayoutRes
-import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import com.learn.soccercleanarchitecture.BR
+import com.learn.soccercleanarchitecture.R
+import com.learn.soccercleanarchitecture.extension.showDialog
 import com.learn.soccercleanarchitecture.util.autoCleared
 import dagger.android.support.DaggerFragment
 import pub.devrel.easypermissions.EasyPermissions
@@ -27,6 +30,8 @@ abstract class BaseFragment<T : ViewDataBinding, V : BaseViewModel> : DaggerFrag
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private var errorMessageDialog: AlertDialog? = null
 
     var viewDataBinding by autoCleared<T>()
 
@@ -51,10 +56,33 @@ abstract class BaseFragment<T : ViewDataBinding, V : BaseViewModel> : DaggerFrag
             executePendingBindings()
             lifecycleOwner = this@BaseFragment
         }
-        observeErrorEvent()
+        subscriberException()
     }
 
-    protected fun observeErrorEvent() {
+    protected open fun subscriberException() {
+        viewModel.run {
+            unexpectedError.observe(viewLifecycleOwner, Observer {
+                handleErrorMessage(message = getString(R.string.unexpected_error))
+            })
+            httpUnavailableError.observe(viewLifecycleOwner, Observer {
+                handleErrorMessage(message = getString(R.string.http_unavailable_error))
+            })
+            rxMapperError.observe(viewLifecycleOwner, Observer {
+                handleErrorMessage(message = getString(R.string.rx_mapper_error))
+            })
+            httpForbiddenError.observe(viewLifecycleOwner, Observer {
+                handleErrorMessage(message = getString(R.string.http_forbidden_error))
+            })
+            httpGatewayTimeoutError.observe(viewLifecycleOwner, Observer {
+                handleErrorMessage(message = getString(R.string.no_internet_error))
+            })
+            errorMessage.observe(viewLifecycleOwner, Observer {
+                handleErrorMessage(message = it)
+            })
+        }
+    }
+
+    private fun handleErrorMessage() {
 
     }
 
@@ -64,5 +92,17 @@ abstract class BaseFragment<T : ViewDataBinding, V : BaseViewModel> : DaggerFrag
 
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
 
+    }
+
+    private fun handleErrorMessage(message: String) {
+        if (errorMessageDialog?.isShowing != true) {
+            errorMessageDialog =
+                context?.showDialog(message = message, positiveMessage = getString(R.string.ok))
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        errorMessageDialog?.dismiss()
     }
 }
